0f5127e61ca13892cb8d115c250fd0c6982f1b71
tchayo <achayof@gmail.com>
2019-12-16 14:52:41 +0300

    controllers

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -1,27 +1,25 @@
 package middlewares
 
 import (
-	"github.com/Tchayo/gotickets/gotickets/api/responses"
 	"errors"
 	"net/http"
 
-	
 	"github.com/Tchayo/gotickets/api/auth"
 	"github.com/Tchayo/gotickets/api/responses"
 )
 
-func SetMiddlewareJSON(next http.HandleFunc) http.HandleFunc {
+func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		next(w, r)
 	}
 }
 
-func SetMiddlewareAuthentication(next http.HandleFunc) http.HandleFunc {
+func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		err := auth.TokenValid(r)
 		if err != nil {
-			responses.ERROR(w http.StatusUnauthorized, errors.New("Unauthorized"))
+			responses.ERROR(w, http.StatusUnauthorized, errors.New("Unauthorized"))
 			return
 		}
 		next(w, r)
---
+++ api/utils/formaterror/formaterror.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"errors"
+	"strings"
+)
+
+func FormatError(err string) error {
+	if strings.Contains(err, "email") {
+		return errors.New("Email Already Taken")
+	}
+
+	if strings.Contains(err, "hashedPassword") {
+		return errors.New("Incorrect Password")
+	}
+	return errors.New("Incorrect Details")
+}
